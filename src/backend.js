c82e37fb86be471008bfad17572fa0a1349e1cd0
Chris Gosser <rivetkopf@gmail.com>
2018-10-08 07:20:59 -0700

    add venus mars and jupiter to the list of planets which you can calculate your age on

--- src/backend.js
+++ src/backend.js
@@ -6,7 +6,13 @@ export class galacticAge {
   checker(){
     return typeof this;
   }
-  mercury(){
-    return this.age * 0.24
+  venus(){
+    return this.age * 0.62
+  }
+  mars(){
+    return this.age * 1.88
+  }
+  jupiter(){
+    return this.age * 11.86
   }
 }
