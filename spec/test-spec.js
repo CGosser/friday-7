ea02b3bbcf690f3db8462a8b1bb7daf6fcbc890b
Chris Gosser <rivetkopf@gmail.com>
2018-10-09 07:19:08 -0700

    create function that calculates life expectancy on other planets

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -8,19 +8,26 @@ let testAge = new galacticAge(34);
   })
 
   it("can convert the ages to those in years of mercury", function(){
-    expect(testAge.mercury()).toEqual(8.16)
+    expect(testAge.mercury()).toEqual(141.67)
   });
 
   it("can convert the ages to those in years of  venus", function(){
-    expect(testAge.venus()).toEqual(21.08)
+    expect(testAge.venus()).toEqual(54.84)
   });
 
   it("can convert the ages to those in years of mars", function(){
-    expect(testAge.mars()).toEqual(63.92)
+    expect(testAge.mars()).toEqual(18.09)
   });
 
   it("can convert the ages to those in years of jupiter", function(){
-    expect(testAge.jupiter()).toEqual(403.24)
+    expect(testAge.jupiter()).toEqual(2.87)
+  });
+
+  it("can determine remaining life expectancy on other planets", function(){
+    expect(testAge.lifeExpectancy("mercury")).toEqual(328.08)
+    expect(testAge.lifeExpectancy("venus")).toEqual(127)
+    expect(testAge.lifeExpectancy("mars")).toEqual(41.88)
+    expect(testAge.lifeExpectancy("jupiter")).toEqual(6.64)
   });
 
 })
--- src/backend.js
+++ src/backend.js
@@ -7,15 +7,30 @@ export class galacticAge {
     return typeof this;
   }
   mercury(){
-    return parseFloat((this.age * 0.24).toFixed(2));
+    return parseFloat((this.age * (1/0.24)).toFixed(2));
   }
   venus(){
-    return parseFloat((this.age * 0.62).toFixed(2));
+    return parseFloat((this.age * (1/0.62)).toFixed(2));
   }
   mars(){
-    return parseFloat((this.age * 1.88).toFixed(2));
+    return parseFloat((this.age * (1/1.88)).toFixed(2));
   }
   jupiter(){
-    return parseFloat((this.age * 11.86).toFixed(2));
+    return parseFloat((this.age * (1/11.86)).toFixed(2));
   }
+  lifeExpectancy(planet){
+    if (planet == "mercury"){
+      return parseFloat((78.74 * (1/0.24)).toFixed(2))
+    }
+    if (planet == "venus"){
+      return parseFloat((78.74 * (1/0.62)).toFixed(2))
+    }
+    if (planet == "mars"){
+      return parseFloat((78.74 * (1/1.88)).toFixed(2))
+    }
+    if (planet == "jupiter"){
+      return parseFloat((78.74 * (1/11.86)).toFixed(2))
+    }
+  }
+
 }
