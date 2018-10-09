73182b34e66e5f49ccc3d39abcd8b3b4c87b8536
Chris Gosser <rivetkopf@gmail.com>
2018-10-09 13:08:29 -0700

    refactor code for DRYness and add test for actuarial function

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -8,26 +8,28 @@ let testAge = new galacticAge(34);
   })
 
   it("can convert the ages to those in years of mercury", function(){
-    expect(testAge.mercury()).toEqual(141.67)
+    expect(testAge.planetAge("mercury")).toEqual(141.67);
   });
 
   it("can convert the ages to those in years of  venus", function(){
-    expect(testAge.venus()).toEqual(54.84)
+    expect(testAge.planetAge("venus")).toEqual(54.84);
   });
 
   it("can convert the ages to those in years of mars", function(){
-    expect(testAge.mars()).toEqual(18.09)
+    expect(testAge.planetAge("mars")).toEqual(18.09);
   });
 
   it("can convert the ages to those in years of jupiter", function(){
-    expect(testAge.jupiter()).toEqual(2.87)
+    expect(testAge.planetAge("jupiter")).toEqual(2.87);
   });
 
   it("can determine remaining life expectancy on other planets", function(){
-    expect(testAge.lifeExpectancy("mercury")).toEqual(328.08)
-    expect(testAge.lifeExpectancy("venus")).toEqual(127)
-    expect(testAge.lifeExpectancy("mars")).toEqual(41.88)
-    expect(testAge.lifeExpectancy("jupiter")).toEqual(6.64)
+    expect(testAge.lifeExpectancy("mercury")).toEqual(328.08);
+    expect(testAge.lifeExpectancy("venus")).toEqual(127);
+    expect(testAge.lifeExpectancy("mars")).toEqual(41.88);
+    expect(testAge.lifeExpectancy("jupiter")).toEqual(6.64);
   });
-
+  it("Determines how many years a user has left to live on each planet", function(){
+    expect(testAge.actuarial("mercury")).toEqual(328.08)
+  })
 })
--- src/backend.js
+++ src/backend.js
@@ -6,61 +6,41 @@ export class galacticAge {
   checker(){
     return typeof this;
   }
-  mercury(){
-    return parseFloat((this.age * (1/0.24)).toFixed(2));
-  }
-  venus(){
-    return parseFloat((this.age * (1/0.62)).toFixed(2));
-  }
-  mars(){
-    return parseFloat((this.age * (1/1.88)).toFixed(2));
-  }
-  jupiter(){
-    return parseFloat((this.age * (1/11.86)).toFixed(2));
-  }
-  lifeExpectancy(planet){
-    if (planet == "mercury"){
-      return parseFloat((78.74 * (1/0.24)).toFixed(2))
-    }
-    if (planet == "venus"){
-      return parseFloat((78.74 * (1/0.62)).toFixed(2))
-    }
-    if (planet == "mars"){
-      return parseFloat((78.74 * (1/1.88)).toFixed(2))
-    }
-    if (planet == "jupiter"){
-      return parseFloat((78.74 * (1/11.86)).toFixed(2))
-    }
-  }
-  actuarial(planet){
-    if (planet == "mercury"){
-      if (mercury() < lifeExpectancy("mercury")) {
-        return lifeExpectancy("mercury") - mercury()
-      } else if (mercury() >= lifeExpectancy("mercury")){
-        return mercury() - lifeExpectancy("mercury")
-      }
-    }
-    if (planet == "venus"){
-      if (venus() < lifeExpectancy("venus")) {
-        return lifeExpectancy("venus") - venus()
-      } else if (venus() >= lifeExpectancy("venus")){
-        return venus() - lifeExpectancy("venus")
-      }
-    }
-    if (planet == "mars"){
-      if (mars() < lifeExpectancy("mars")) {
-        return lifeExpectancy("mars") - mars()
-      } else if (mars() >= lifeExpectancy("mars")){
-        return mars() - lifeExpectancy("mars")
-      }
-    }
-    if (planet == "jupiter"){
-      if (jupiter() < lifeExpectancy("jupiter")) {
-        return lifeExpectancy("jupiter") - jupiter()
-      } else if (jupiter() >= lifeExpectancy("jupiter")){
-        return jupiter() - lifeExpectancy("jupiter")
-      }
-    }
 
+  planetConverter(planet, age) {
+    const mercury = (1/0.24);
+    const venus = (1/0.62);
+    const mars = (1/1.88);
+    const jupiter = (1/11.86);
+    if (planet == "mercury"){
+      return parseFloat((age * mercury).toFixed(2));
+    }
+    if (planet == "venus"){
+      return parseFloat((age * venus).toFixed(2));
+    }
+    if (planet == "mars"){
+      return parseFloat((age * mars).toFixed(2));
+    }
+    if (planet == "jupiter"){
+      return parseFloat((age * jupiter).toFixed(2));
+    }
   }
-}
+
+  planetAge(planet) {
+    return this.planetConverter(planet, this.age);
+  }
+
+  lifeExpectancy(planet){
+    return this.planetConverter(planet, 78.74);
+  }
+
+  actuarial(planet){
+    const planetaryAge = this.planetAge(planet)
+    const lifeExpectancy = this.lifeExpectancy(planet)
+      if ( planetaryAge < lifeExpectancy) {
+        return lifeExpectancy - planetaryAge;
+      } else if (planetaryAge >= lifeExpectancy){
+        return planetaryAge - lifeExpectancy;
+      }
+    }
+  }
