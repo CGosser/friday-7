f4d5c847df0390421f8f228fab51f457813b7697
Chris Gosser <rivetkopf@gmail.com>
2018-10-09 07:26:20 -0700

    create function which should return the years left to live of a person in the years of each planet

--- src/backend.js
+++ src/backend.js
@@ -32,5 +32,35 @@ export class galacticAge {
       return parseFloat((78.74 * (1/11.86)).toFixed(2))
     }
   }
+  actuarial(planet){
+    if (planet == "mercury"){
+      if (mercury() < lifeExpectancy("mercury")) {
+        return lifeExpectancy("mercury") - mercury()
+      } else if (mercury() >= lifeExpectancy("mercury")){
+        return mercury() - lifeExpectancy("mercury")
+      }
+    }
+    if (planet == "venus"){
+      if (venus() < lifeExpectancy("venus")) {
+        return lifeExpectancy("venus") - venus()
+      } else if (venus() >= lifeExpectancy("venus")){
+        return venus() - lifeExpectancy("venus")
+      }
+    }
+    if (planet == "mars"){
+      if (mars() < lifeExpectancy("mars")) {
+        return lifeExpectancy("mars") - mars()
+      } else if (mars() >= lifeExpectancy("mars")){
+        return mars() - lifeExpectancy("mars")
+      }
+    }
+    if (planet == "jupiter"){
+      if (jupiter() < lifeExpectancy("jupiter")) {
+        return lifeExpectancy("jupiter") - jupiter()
+      } else if (jupiter() >= lifeExpectancy("jupiter")){
+        return jupiter() - lifeExpectancy("jupiter")
+      }
+    }
 
+  }
 }
